98a784030bde6d40e027d9588afaab1c011edc75
Mace <maisonarroyo@protonmail.com>
2022-04-21 13:56:58 -0400

    // Report // - Added problem type to instantiation list - added two methods

--- Models/Report.cs
+++ Models/Report.cs
@@ -6,8 +6,55 @@
         public long UID = 0;
         public long IDToReport = 0;
         public bool Resolved = false;
+        public ProblemTypes ProblemType = ProblemTypes.NoType;
 
-        public enum ProblemTypes 
+
+
+		public string ProblemText
+		{
+			get
+			{
+				switch (ProblemType)
+				{
+					case ProblemTypes.MisleadingorScam:
+						return "Missleading or Scam";
+					case ProblemTypes.SexuallyInappropriate:
+						return "Sexually Inappropriate";
+					case ProblemTypes.Offensive:
+						return "Offensive";
+					case ProblemTypes.Violent:
+						return "Violent";
+					case ProblemTypes.Spam:
+						return "Spam";
+					default: //NoType or anything else
+						return "Not set";
+				}
+			}
+		}
+
+		public static string GetProblemText(ProblemTypes pt)
+		{
+			switch (pt)
+			{
+				case ProblemTypes.MisleadingorScam:
+					return "Missleading or Scam";
+				case ProblemTypes.SexuallyInappropriate:
+					return "Sexually Inappropriate";
+				case ProblemTypes.Offensive:
+					return "Offensive";
+				case ProblemTypes.Violent:
+					return "Violent";
+				case ProblemTypes.Spam:
+					return "Spam";
+				default: //NoType or anything else
+					return "Not set";
+			}
+		}
+
+
+
+
+		public enum ProblemTypes 
         {  
             NoType = 0,
             MisleadingorScam = 1,
